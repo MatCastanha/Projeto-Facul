77fbb5bbb66c958ff8953602d29c3fc944d42aaa
Matheus Castanha <matheuscastrod33@gmail.com>
2024-05-16 20:00:42 -0300

    Add files via upload

---
+++ Aula 16.05.24/While.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+	//while = enquanto		
+	
+	// Declaração contador
+	// while (condição de parada)
+	// {
+	//	  incremento ou decremento ==> EVITA PROGRAMA EM LOOPING
+	// }
+	
+	int contador=1;
+	while(contador<=10){
+		printf("%i - linguagem C\n",contador);
+		contador+=1;
+	}
+	
+	return 0;
+}
